a7a5970035bccc463a018ac2a71a2b35143b6e6f
Sercan Akmaz <sercan.akmaz@gmail.com>
2016-10-26 15:04:07 +0300

    } bugfix

--- Test.Web/Global.asax.cs
+++ Test.Web/Global.asax.cs
@@ -13,7 +13,8 @@ namespace Test.Web
 
         protected void Application_Start(object sender, EventArgs e)
         {
-     
+
+        }
 
         protected void Session_Start(object sender, EventArgs e)
         {
